d69392221d56148e644ceb277144098bf8c2d9f8
Stef Buurman <St3fbuurman@gmail.com>
2024-10-15 13:41:29 +0200

    Import path named better

--- api/providers/data_provider.py
+++ api/providers/data_provider.py
@@ -29,62 +29,62 @@ def init():
 
 # Load data for each model
 def get_items():
-    from ..models.items import Items
+    from api.models.items import Items
     global _items
     _items = Items(ROOT_PATH, DEBUG)
 
 def get_item_lines():
-    from ..models.item_lines import ItemLines
+    from api.models.item_lines import ItemLines
     global _item_lines
     _item_lines = ItemLines(ROOT_PATH, DEBUG)
 
 def get_item_groups():
-    from ..models.item_groups import ItemGroups
+    from api.models.item_groups import ItemGroups
     global _item_groups
     _item_groups = ItemGroups(ROOT_PATH, DEBUG)
 
 def get_item_types():
-    from ..models.item_types import ItemTypes
+    from api.models.item_types import ItemTypes
     global _item_types
     _item_types = ItemTypes(ROOT_PATH, DEBUG)
 
 def get_warehouses():
-    from ..models.warehouses import Warehouses
+    from api.models.warehouses import Warehouses
     global _warehouses
     _warehouses = Warehouses(ROOT_PATH, DEBUG)
 
 def get_locations():
-    from ..models.locations import Locations
+    from api.models.locations import Locations
     global _locations
     _locations = Locations(ROOT_PATH, DEBUG)
 
 def get_transfers():
-    from ..models.transfers import Transfers
+    from api.models.transfers import Transfers
     global _transfers
     _transfers = Transfers(ROOT_PATH, DEBUG)
 
 def get_clients():
-    from ..models.clients import Clients
+    from api.models.clients import Clients
     global _clients
     _clients = Clients(ROOT_PATH, DEBUG)
 
 def get_shipments():
-    from ..models.shipments import Shipments
+    from api.models.shipments import Shipments
     global _shipments
     _shipments = Shipments(ROOT_PATH, DEBUG)
 
 def get_suppliers():
-    from ..models.suppliers import Suppliers
+    from api.models.suppliers import Suppliers
     global _suppliers
     _suppliers = Suppliers(ROOT_PATH, DEBUG)
 
 def get_inventories():
-    from ..models.inventories import Inventories
+    from api.models.inventories import Inventories
     global _inventories
     _inventories = Inventories(ROOT_PATH, DEBUG)
 
 def get_orders():
-    from ..models.orders import Orders
+    from api.models.orders import Orders
     global _orders
     _orders = Orders(ROOT_PATH, DEBUG)
 
