3ea137d20dbeab3b59cbb341ff2e5b64517b5495
Stef Buurman <St3fbuurman@gmail.com>
2024-10-17 13:54:05 +0200

    Added variables to test_globals

--- tests/integration/test_globals.py
+++ tests/integration/test_globals.py
@@ -1,1 +1,4 @@
 MAIN_URL = "http://localhost:8000/api/v1"
+test_headers = {"API_KEY": "test_api_key"}
+invalid_headers = {"API_KEY": "invalid_api_key"}
+never_existing_id = "99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999";
