5b59b051a1a53da796a5e9e526a7cc6b3bdbf76d
Roel van Loon <van.loon.roel@gmail.com>
2024-10-17 10:36:43 +0200

    shipmentcontroller Fix

--- api/Controllers/ShipmentController.py
+++ api/Controllers/ShipmentController.py
@@ -5,7 +5,7 @@ from providers import data_provider, auth_provider
 shipment_router = APIRouter()
 
 @shipment_router.get("/{shipment_id}")
-def read_shipment(shipment_id: str, api_key: str = Depends(auth_provider.get_api_key)):
+def read_shipment(shipment_id: int, api_key: str = Depends(auth_provider.get_api_key)):
     data_provider.init()
     shipments = data_provider.fetch_shipment_pool().get_shipment(shipment_id)
     if shipments in None:
@@ -21,19 +21,22 @@ def read_shipments(api_key: str = Depends(auth_provider.get_api_key)):
 @shipment_router.post("/")
 def create_shipment(shipment: dict, api_key: str = Depends(auth_provider.get_api_key)):
     data_provider.init()
+    existingShipment = data_provider.fetch_shipment_pool().get_shipment(shipment["id"])
+    if existingShipment is not None:
+        raise HTTPException(status_code=409, detail="shipment already exists")
     data_provider.fetch_shipment_pool().add_shipment(shipment)
-    data_provider.fetch_item_pool().save()
-    return shipment
+    data_provider.fetch_shipment_pool().save()
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=shipment)
 
 @shipment_router.put("/{shipment_id}")
-def update_shipment(shipment_id: str, shipment: dict, api_key: str = Depends(auth_provider.get_api_key)):
+def update_shipment(shipment_id: int, shipment: dict, api_key: str = Depends(auth_provider.get_api_key)):
     data_provider.init()
     data_provider.fetch_shipment_pool().update_shipment(shipment_id, shipment)
     data_provider.fetch_shipment_pool().save()
     return shipment
 
 @shipment_router.delete("/{shipment_id}")
-def delete_shipment(shipment_id: str, api_key: str = Depends(auth_provider.get_api_key)):
+def delete_shipment(shipment_id: int, api_key: str = Depends(auth_provider.get_api_key)):
     data_provider.init()
     shipment_pool = data_provider.fetch_shipment_pool()
 
