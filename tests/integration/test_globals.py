5fe70259e94aaa08f194a09f040722387a109fa9
NickWijmans <nickwijmans@gmail.com>
2024-10-17 14:43:11 +0200

    Added variables to test globals + improved tests for warehouses

--- tests/integration/test_globals.py
+++ tests/integration/test_globals.py
@@ -1,1 +1,7 @@
 MAIN_URL = "http://localhost:8000/api/v1"
+
+non_existent_id = 99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
+
+test_headers = {"API_KEY": "test_api_key"}
+
+invalid_headers = {"API_KEY": "invalid_key"}
