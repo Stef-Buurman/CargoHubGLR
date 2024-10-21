23ddf44cda24387e1ca32349b24a5eac6a40cc4e
1070408 <1070408@hr.nl>
2024-10-21 16:34:56 +0200

    Added integration tests for item_lines

---
+++ tests/integration/test_integration_item_lines.py
@@ -0,0 +1,183 @@
+import pytest
+import httpx
+from test_globals import * 
+
+test_item_line = {
+    "id": 999,
+    "name": "Test Item Line",
+    "description": "",
+    "created_at": "2022-08-18 07:05:25",
+    "updated_at": "2023-05-15 15:44:28"
+}
+
+
+@pytest.fixture
+def client():
+    with httpx.Client(base_url=MAIN_URL) as client:
+        yield client
+
+
+def test_get_all_item_lines(client):
+    response = client.get('/item_lines/', headers=test_headers)
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+
+def test_get_all_item_lines_no_api_key(client):
+    response = client.get('/item_lines/')
+    assert response.status_code == 403
+
+
+def test_get_all_item_lines_invalid_api_key(client):
+    response = client.get('/item_lines/', headers=invalid_headers)
+    assert response.status_code == 403
+
+
+def test_add_item_line_no_api_key(client):
+    response = client.post('/item_lines/', json=test_item_line)
+    assert response.status_code == 403
+    responseGet = client.get('/item_lines/' + str(test_item_line['id']), headers=test_headers)
+    assert responseGet.status_code == 404
+
+
+def test_add_item_line_invalid_api_key(client):
+    response = client.post('/item_lines/', json=test_item_line, headers=invalid_headers)
+    assert response.status_code == 403
+    responseGet = client.get('/item_lines/' + str(test_item_line['id']), headers=test_headers)
+    assert responseGet.status_code == 404
+
+
+def test_add_item_line(client):
+    response = client.post('/item_lines/', json=test_item_line, headers=test_headers)
+    assert response.status_code == 201 or response.status_code == 200
+    assert response.json()["id"] == test_item_line["id"]
+
+
+def test_add_existing_item_line(client):
+    response = client.post('/item_lines/', json=test_item_line, headers=test_headers)
+    assert response.status_code == 409
+
+
+def test_get_item_line_by_id(client):
+    response = client.get('/item_lines/' + str(test_item_line['id']), headers=test_headers)
+    assert response.status_code == 200
+    assert response.json() is not None
+    assert isinstance(response.json(), dict)
+    assert response.json()['id'] == test_item_line['id']
+    
+
+def test_get_item_line_by_invalid_id(client):
+    response = client.get('/item_lines/asdabsdasdhasj', headers=test_headers)
+    assert response.status_code == 422
+
+
+def test_get_item_line_non_existent_id(client):
+    response = client.get('/item_lines/' + str(non_existent_id), headers=test_headers)
+    assert response.status_code == 404
+
+
+def test_get_item_line_no_api_key(client):
+    response = client.get('/item_lines/' + str(test_item_line['id']))
+    assert response.status_code == 403
+
+
+def test_get_item_line_invalid_api_key(client):
+    response = client.get('/item_lines/' + str(test_item_line['id']), headers=invalid_headers)
+    assert response.status_code == 403
+
+
+def test_get_item_line_items_no_items(client):
+    response = client.get(f'/item_lines/{str(test_item_line['id'])}/items', headers=test_headers)
+    assert response.status_code == 204
+
+
+def test_get_item_line_items_no_api_key(client):
+    response = client.get(f'/item_lines/{str(test_item_line['id'])}/items')
+    assert response.status_code == 403
+
+
+def test_get_item_line_items_invalid_api_key(client):
+    response = client.get(f'/item_lines/{str(test_item_line['id'])}/items', headers=invalid_headers)
+    assert response.status_code == 403
+
+
+def test_get_item_line_items_invalid_id(client):
+    response = client.get('/item_lines/invalid_id/items', headers=test_headers)
+    assert response.status_code == 422
+
+
+def test_update_item_line_no_api_key(client):
+    updated_item_line = test_item_line.copy()
+    updated_item_line['description'] = 'updated item line'
+    response = client.put('/item_lines/' + str(updated_item_line['id']), json=updated_item_line)
+    assert response.status_code == 403
+    response_get_item_line = client.get('/item_lines/' + str(updated_item_line['id']), headers=test_headers)
+    assert response_get_item_line.status_code == 200
+    assert response_get_item_line.json()['description'] == test_item_line['description']
+
+
+def test_update_item_line_invalid_api_key(client):
+    updated_item_line = test_item_line.copy()
+    updated_item_line['description'] = 'updated item line'
+    response = client.put('/item_lines/' + str(updated_item_line['id']), json=updated_item_line, headers=invalid_headers)
+    assert response.status_code == 403
+    response_get_item_line = client.get('/item_lines/' + str(updated_item_line['id']), headers=test_headers)
+    assert response_get_item_line.status_code == 200
+    assert response_get_item_line.json()['description'] == test_item_line['description']
+
+
+def test_update_item_line_invalid_id(client):
+    updated_item_line = test_item_line.copy()
+    updated_item_line['description'] = 'updated item line'
+    response = client.put('/item_lines/invalid_id', json=updated_item_line, headers=test_headers)
+    assert response.status_code == 422
+
+
+def test_update_item_line_non_existent_id(client):
+    updated_item_line = test_item_line.copy()
+    updated_item_line['description'] = 'updated item line'
+    response = client.put('/item_lines/' + str(non_existent_id), json=updated_item_line, headers=test_headers)
+    assert response.status_code == 404
+
+
+def test_update_item_line(client):
+    updated_item_line = test_item_line.copy()
+    updated_item_line['description'] = 'updated item line'
+    response = client.put('/item_lines/' + str(updated_item_line['id']), json=updated_item_line, headers=test_headers)
+    assert response.status_code == 200
+    response_get_item_line = client.get('/item_lines/' + str(updated_item_line['id']), headers=test_headers)
+    assert response_get_item_line.status_code == 200
+    assert response_get_item_line.json()['description'] == updated_item_line['description']
+
+
+def test_delete_item_line_no_api_key(client):
+    response = client.delete('/item_lines/' + str(test_item_line['id']))
+    assert response.status_code == 403
+    response_get_item_line = client.get('/item_lines/' + str(test_item_line['id']), headers=test_headers)
+    assert response_get_item_line.status_code == 200
+
+
+def test_delete_item_line_invalid_api_key(client):
+    response = client.delete('/item_lines/' + str(test_item_line['id']), headers=invalid_headers)
+    assert response.status_code == 403
+    response_get_item_line = client.get('/item_lines/' + str(test_item_line['id']), headers=test_headers)
+    assert response_get_item_line.status_code == 200
+
+
+def test_delete_item_line_invalid_id(client):
+    response = client.delete('/item_lines/invalid_id', headers=test_headers)
+    assert response.status_code == 422
+
+
+def test_delete_item_line_non_existent_id(client):
+    response = client.delete('/item_lines/' + str(non_existent_id), headers=test_headers)
+    assert response.status_code == 404
+    response_get_item_line = client.get('/item_lines/' + str(test_item_line['id']), headers=test_headers)
+    assert response_get_item_line.status_code == 200
+
+
+def test_delete_item_line(client):
+    response = client.delete('/item_lines/' + str(test_item_line['id']), headers=test_headers)
+    assert response.status_code == 200
+    response_get_item_line = client.get('/item_lines/' + str(test_item_line['id']), headers=test_headers)
+    assert response_get_item_line.status_code == 404
