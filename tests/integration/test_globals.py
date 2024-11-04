fbaa8399b363262b7d914fa6dd001145c5ee0f4d
Stef <st3fbuurman@gmail.com>
2024-11-04 14:09:43 +0100

    Changed environment to Authorization

--- tests/integration/test_globals.py
+++ tests/integration/test_globals.py
@@ -2,6 +2,6 @@ MAIN_URL = "http://localhost:8000/api/v1"
 
 non_existent_id = 99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
 
-test_headers = {"Environment": "test_api_key"}
+test_headers = {"Authorization": "test_api_key"}
 
-invalid_headers = {"Environment": "invalid_key"}
+invalid_headers = {"Authorization": "invalid_key"}
