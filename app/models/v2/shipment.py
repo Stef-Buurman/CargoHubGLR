36ee1e724862303cf336bb9105a32cf73433b5fb
Stef Buurman <St3fbuurman@gmail.com>
2024-11-06 12:55:15 +0100

    Created shipment model v2 version

---
+++ app/models/v2/shipment.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from typing import List
+from models.v2.ItemInObject import ItemInObject
+
+
+class Shipment(BaseModel):
+    id: int | None = None
+    order_id: int
+    source_id: int
+    order_date: str
+    request_date: str
+    shipment_date: str
+    shipment_type: str
+    shipment_status: str
+    notes: str | None = None
+    carrier_code: str
+    carrier_description: str
+    service_code: str
+    payment_type: str
+    transfer_mode: str
+    total_package_count: int
+    total_package_weight: float
+    created_at: str | None = None
+    updated_at: str | None = None
+    items: List[ItemInObject]
