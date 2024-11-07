29ca0f88f1b2dfc62608730b8735e31cf9d1cb1f
Stef Buurman <St3fbuurman@gmail.com>
2024-11-07 11:01:37 +0100

    Created service for inventories and used it inside dataprovide_v2

---
+++ app/services/InventoryService.py
@@ -0,0 +1,74 @@
+import json
+from typing import List
+from models.v2.inventory import Inventory
+from models.base import Base
+
+INVENTORIES = []
+
+
+class InventoryService(Base):
+    def __init__(self, root_path, is_debug=False):
+        self.data_path = root_path + "inventories.json"
+        self.load(is_debug)
+
+    def get_inventories(self) -> List[Inventory]:
+        return self.data
+
+    def get_inventory(self, inventory_id: int) -> Inventory | None:
+        for x in self.data:
+            if x.id == inventory_id:
+                return x
+        return None
+
+    def get_inventories_for_item(self, item_id: str) -> List[Inventory]:
+        result = []
+        for x in self.data:
+            if x.item_id == item_id:
+                result.append(x)
+        return result
+
+    def get_inventory_totals_for_item(self, item_id: str) -> dict:
+        result = {
+            "total_expected": 0,
+            "total_ordered": 0,
+            "total_allocated": 0,
+            "total_available": 0,
+        }
+        for x in self.data:
+            if x.item_id == item_id:
+                result["total_expected"] += x.total_expected
+                result["total_ordered"] += x.total_ordered
+                result["total_allocated"] += x.total_allocated
+                result["total_available"] += x.total_available
+        return result
+
+    def add_inventory(self, inventory: Inventory) -> Inventory:
+        inventory.created_at = self.get_timestamp()
+        inventory.updated_at = self.get_timestamp()
+        self.data.append(inventory)
+        return inventory
+
+    def update_inventory(self, inventory_id: int, inventory: Inventory) -> Inventory:
+        inventory.updated_at = self.get_timestamp()
+        for i in range(len(self.data)):
+            if self.data[i].id == inventory_id:
+                self.data[i] = inventory
+                break
+        return inventory
+
+    def remove_inventory(self, inventory_id: int):
+        for x in self.data:
+            if x.id == inventory_id:
+                self.data.remove(x)
+
+    def load(self, is_debug: bool, inventories: List[Inventory] | None = None):
+        if is_debug and inventories is not None:
+            self.data = inventories
+        else:
+            with open(self.data_path, "r") as f:
+                raw_data = json.load(f)
+                self.data = [Inventory(**inventory_dict) for inventory_dict in raw_data]
+
+    def save(self):
+        with open(self.data_path, "w") as f:
+            json.dump([inventory.model_dump() for inventory in self.data], f)
