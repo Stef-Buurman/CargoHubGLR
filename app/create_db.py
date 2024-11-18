36c9c0f25b9a2d2bbe734679be619ac4fe59263c
Stef Buurman <St3fbuurman@gmail.com>
2024-11-18 13:57:11 +0100

    Fix typo in elapsed time print statement

--- app/create_db.py
+++ app/create_db.py
@@ -159,4 +159,4 @@ if __name__ == "__main__":
 
     elapsed_seconds = elapsed_time 
 
-    print(f"The script ran for {elapsed_seconds:.2f} xeconds.")
+    print(f"The script ran for {elapsed_seconds:.2f} seconds.")
