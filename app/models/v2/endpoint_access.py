788abaf0d5aec866eea56c89c18a2b037134c725
Stef Buurman <St3fbuurman@gmail.com>
2024-11-20 14:57:12 +0100

    fixed models

--- app/models/v2/endpoint_access.py
+++ app/models/v2/endpoint_access.py
@@ -1,24 +1,12 @@
 from models.v2.base import Base
 
-class EndpointAccessDetail(Base):
+class EndpointAccess(Base):
+    class Config:
+        table_name = "endpoint_access"
+
+    endpoint: str
     full: bool
     get: bool | None = None
     post: bool | None = None
     put: bool | None = None
     delete: bool | None = None
-
-class EndpointAccess(Base):
-    class Config:
-        table_name = "endpoint_access"
-
-    warehouses: EndpointAccessDetail | None = None
-    locations: EndpointAccessDetail | None = None
-    transfers: EndpointAccessDetail | None = None
-    items: EndpointAccessDetail | None = None
-    item_lines: EndpointAccessDetail | None = None
-    item_groups: EndpointAccessDetail | None = None
-    item_types: EndpointAccessDetail | None = None
-    suppliers: EndpointAccessDetail | None = None
-    orders: EndpointAccessDetail | None = None
-    clients: EndpointAccessDetail | None = None
-    shipments: EndpointAccessDetail | None = None
