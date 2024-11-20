cc38967c71d58cb8b60061cdb6b8fa2f34dda3a6
Stef Buurman <St3fbuurman@gmail.com>
2024-11-20 14:09:54 +0100

    Fixed v1 auth provider

--- app/services/auth_provider.py
+++ app/services/auth_provider.py
@@ -1,102 +1,7 @@
 from fastapi import HTTPException, Request, Security
 from fastapi.security import APIKeyHeader
 
-USERS = [
-    {
-        "api_key": "test_api_key",
-        "app": "Integration_Tests",
-        "endpoint_access": {"full": True},
-    },
-    {
-        "api_key": "a1b2c3d4e5",
-        "app": "CargoHUB Dashboard 1",
-        "endpoint_access": {"full": True},
-    },
-    {
-        "api_key": "f6g7h8i9j0",
-        "app": "CargoHUB Dashboard 2",
-        "endpoint_access": {
-            "full": False,
-            "warehouses": {
-                "full": False,
-                "get": True,
-                "post": False,
-                "put": False,
-                "delete": False,
-            },
-            "locations": {
-                "full": False,
-                "get": True,
-                "post": False,
-                "put": False,
-                "delete": False,
-            },
-            "transfers": {
-                "full": False,
-                "get": True,
-                "post": False,
-                "put": False,
-                "delete": False,
-            },
-            "items": {
-                "full": False,
-                "get": True,
-                "post": False,
-                "put": False,
-                "delete": False,
-            },
-            "item_lines": {
-                "full": False,
-                "get": True,
-                "post": False,
-                "put": False,
-                "delete": False,
-            },
-            "item_groups": {
-                "full": False,
-                "get": True,
-                "post": False,
-                "put": False,
-                "delete": False,
-            },
-            "item_types": {
-                "full": False,
-                "get": True,
-                "post": False,
-                "put": False,
-                "delete": False,
-            },
-            "suppliers": {
-                "full": False,
-                "get": True,
-                "post": False,
-                "put": False,
-                "delete": False,
-            },
-            "orders": {
-                "full": False,
-                "get": True,
-                "post": False,
-                "put": False,
-                "delete": False,
-            },
-            "clients": {
-                "full": False,
-                "get": True,
-                "post": False,
-                "put": False,
-                "delete": False,
-            },
-            "shipments": {
-                "full": False,
-                "get": True,
-                "post": False,
-                "put": False,
-                "delete": False,
-            },
-        },
-    },
-]
+USERS = [{"api_key": "test_api_key", "app": "Integration_Tests", "endpoint_access": {"full": True}}, {"api_key": "a1b2c3d4e5", "app": "CargoHUB Dashboard 1", "endpoint_access": {"full": True}}, {"api_key": "f6g7h8i9j0", "app": "CargoHUB Dashboard 2", "endpoint_access": {"full": False, "warehouses": {"full": False, "get": True, "post": False, "put": False, "delete": False}, "locations": {"full": False, "get": True, "post": False, "put": False, "delete": False}, "transfers": {"full": False, "get": True, "post": False, "put": False, "delete": False}, "items": {"full": False, "get": True, "post": False, "put": False, "delete": False}, "item_lines": {"full": False, "get": True, "post": False, "put": False, "delete": False}, "item_groups": {"full": False, "get": True, "post": False, "put": False, "delete": False}, "item_types": {"full": False, "get": True, "post": False, "put": False, "delete": False}, "suppliers": {"full": False, "get": True, "post": False, "put": False, "delete": False}, "orders": {"full": False, "get": True, "post": False, "put": False, "delete": False}, "clients": {"full": False, "get": True, "post": False, "put": False, "delete": False}, "shipments": {"full": False, "get": True, "post": False, "put": False, "delete": False}}}]
 
 _users = USERS
 
@@ -132,9 +37,9 @@ def get_api_key(request: Request, api_key_header: str = Security(api_key_header)
 
     if user is None:
         raise HTTPException(status_code=403, detail="Invalid API Key")
-    
-    segments = request.url.path.strip('/').split('/')
-    third_item = segments[2] if len(segments) > 2 else None  
+
+    segments = request.url.path.strip("/").split("/")
+    third_item = segments[2] if len(segments) > 2 else None
     if third_item == None:
         raise HTTPException(status_code=400)
 
