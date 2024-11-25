b8e916f13cd52b51054786381a7e0b8f383df5c5
Roel van Loon <contact@roelvanloon.com>
2024-11-25 15:21:55 +0100

    Fix typo in get_shipments method return type annotation

--- app/services/shipment_service.py
+++ app/services/shipment_service.py
@@ -16,7 +16,7 @@ class ShipmentService(Base):
         self.current_id = 0
         self.db = DB
 
-    def get_shipments(self) -> List[Shipmen]:
+    def get_shipments(self) -> List[Shipment]:
         return self.data
 
     def get_shipment(self, shipment_id: str) -> Optional[Shipment]:
