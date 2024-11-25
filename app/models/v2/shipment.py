7ec3b4d471370e0d3160d4616b47f3b4c41f63c1
Roel van Loon <contact@roelvanloon.com>
2024-11-25 15:07:54 +0100

    Remove unused attributes from Shipment class to streamline model definition

--- app/models/v2/shipment.py
+++ app/models/v2/shipment.py
@@ -22,25 +22,3 @@ class Shipment(Base):
     created_at: str | None = None
     updated_at: str | None = None
     items: list[ItemInObject]
-
-
-class ShipmentDB(Base):
-    id: int | None = None
-    order_id: int
-    source_id: int
-    order_date: str
-    request_date: str
-    shipment_date: str
-    shipment_type: str
-    shipment_status: str
-    notes: str | None = None
-    carrier_code: str
-    carrier_description: str
-    service_code: str
-    payment_type: str
-    transfer_mode: str
-    total_package_count: int
-    total_package_weight: float
-    created_at: str | None = None
-    updated_at: str | None = None
-    items: list[ItemInObject]
