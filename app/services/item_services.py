62fb71772bff12bf3845c087758d9a02734aa339
Roel van Loon <contact@roelvanloon.com>
2024-11-26 10:45:09 +0100

    Update ItemService constructor type hints for clarity

--- app/services/item_services.py
+++ app/services/item_services.py
@@ -8,7 +8,7 @@ ITEMS = []
 
 
 class ItemService(Base):
-    def __init__(self, is_debug=False, items=None):
+    def __init__(self, is_debug=bool, items: list[Item] | None = None):
         self.db = DB
         self.load(is_debug, items)
 
