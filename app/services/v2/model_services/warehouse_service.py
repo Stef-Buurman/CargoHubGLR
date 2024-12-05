6550d3e31641537b9d9e065c7f8ec26f58671e12
NickWijmans <NickWijmans@gmail.com>
2024-12-05 12:58:23 +0100

    Implement archive and unarchive functionality for warehouses in API and service layer

--- app/services/v2/model_services/warehouse_service.py
+++ app/services/v2/model_services/warehouse_service.py
@@ -13,12 +13,21 @@ class WarehouseService(Base):
         self.db = DB
         self.load(is_debug, warehouses)
 
-    def get_warehouses(self) -> List[WarehouseDB]:
+    def get_all_warehouses(self) -> List[WarehouseDB]:
         return self.db.get_all(WarehouseDB)
+
+    def get_warehouses(self) -> List[WarehouseDB]:
+        warehouses = []
+        for warehouse in self.data:
+            if not warehouse.is_archived:
+                warehouses.append(warehouse)
+        return warehouses
 
     def get_warehouse(self, warehouse_id: int) -> WarehouseDB | None:
         for warehouse in self.data:
             if warehouse.id == warehouse_id:
+                if warehouse.is_archived:
+                    return None
                 return warehouse
         return self.db.get(WarehouseDB, warehouse_id)
 
@@ -36,15 +45,29 @@ class WarehouseService(Base):
         warehouse.updated_at = self.get_timestamp()
         for i in range(len(self.data)):
             if self.data[i].id == warehouse_id:
+                if warehouse.is_archived:
+                    return None
                 self.data[i] = warehouse
                 break
         return self.db.update(warehouse, warehouse_id, closeConnection)
 
-    def remove_warehouse(self, warehouse_id: int, closeConnection: bool = True) -> bool:
+    def archive_warehouse(
+        self, warehouse_id: int, closeConnection: bool = True
+    ) -> bool:
         for warehouse in self.data:
             if warehouse.id == warehouse_id:
-                if self.db.delete(WarehouseDB, warehouse_id, closeConnection):
-                    self.data.remove(warehouse)
+                warehouse.is_archived = True
+                if self.db.update(warehouse, warehouse_id, closeConnection):
+                    return True
+        return False
+
+    def unarchive_warehouse(
+        self, warehouse_id: int, closeConnection: bool = True
+    ) -> bool:
+        for warehouse in self.data:
+            if warehouse.id == warehouse_id:
+                warehouse.is_archived = False
+                if self.db.update(warehouse, warehouse_id, closeConnection):
                     return True
         return False
 
@@ -52,4 +75,10 @@ class WarehouseService(Base):
         if is_debug and warehouses is not None:
             self.data = warehouses
         else:
-            self.data = self.get_warehouses()
+            self.data = self.get_all_warehouses()
+
+    def is_warehouse_archived(self, warehouse_id: int) -> bool:
+        for warehouse in self.data:
+            if warehouse.id == warehouse_id:
+                return warehouse.is_archived
+        return None
