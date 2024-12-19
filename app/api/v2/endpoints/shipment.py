c8c3eb499b002c07338cea2f22a9331424745526
NickWijmans <NickWijmans@gmail.com>
2024-12-19 13:34:50 +0100

    Refactor shipment endpoints to use request state for pagination and remove unnecessary blank lines

--- app/api/v2/endpoints/shipment.py
+++ app/api/v2/endpoints/shipment.py
@@ -1,19 +1,16 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Request, HTTPException, status
 from fastapi.responses import JSONResponse
 from models.v2.order import Order
 from models.v2.ItemInObject import ItemInObject
-from services.v2.pagination_service import Pagination
 from services.v2 import data_provider_v2
 from models.v2.shipment import Shipment
 from typing import List
-from utils.globals import pagination_url
 
 shipment_router_v2 = APIRouter(tags=["v2.Shipments"], prefix="/shipments")
 
 
 @shipment_router_v2.get("/{shipment_id}")
 def read_shipment(shipment_id: int):
-
     shipment = data_provider_v2.fetch_shipment_pool().get_shipment(shipment_id)
     if shipment is None:
         raise HTTPException(
@@ -23,44 +20,37 @@ def read_shipment(shipment_id: int):
 
 
 @shipment_router_v2.get("/")
-@shipment_router_v2.get(pagination_url)
-def read_shipments(pagination: Pagination = Depends()):
-
+def read_shipments(request: Request):
     shipments = data_provider_v2.fetch_shipment_pool().get_shipments()
     if shipments is None:
         raise HTTPException(status_code=404, detail="No shipments found")
-    return pagination.apply(shipments)
+    return request.state.pagination.apply(shipments)
 
 
 @shipment_router_v2.get("/{shipment_id}/orders")
-@shipment_router_v2.get("/{shipment_id}/orders" + pagination_url)
-def read_orders_for_shipment(shipment_id: int, pagination: Pagination = Depends()):
-
+def read_orders_for_shipment(shipment_id: int, request: Request):
     shipment = data_provider_v2.fetch_shipment_pool().get_shipment(shipment_id)
     if shipment is None:
         raise HTTPException(
             status_code=404, detail=f"Shipment with id {shipment_id} not found"
         )
     orders = data_provider_v2.fetch_order_pool().get_orders_for_shipments(shipment_id)
-    return pagination.apply(orders)
+    return request.state.pagination.apply(orders)
 
 
 @shipment_router_v2.get("/{shipment_id}/items")
-@shipment_router_v2.get("/{shipment_id}/items" + pagination_url)
-def read_items_for_shipment(shipment_id: int, pagination: Pagination = Depends()):
-
+def read_items_for_shipment(shipment_id: int, request: Request):
     shipment = data_provider_v2.fetch_shipment_pool().get_shipment(shipment_id)
     if shipment is None:
         raise HTTPException(
             status_code=404, detail=f"Shipment with id {shipment_id} not found"
         )
     items = data_provider_v2.fetch_shipment_pool().get_items_in_shipment(shipment_id)
-    return pagination.apply(items)
+    return request.state.pagination.apply(items)
 
 
 @shipment_router_v2.post("/")
 def create_shipment(shipment: Shipment):
-
     created_shipment = data_provider_v2.fetch_shipment_pool().add_shipment(shipment)
     return JSONResponse(
         status_code=status.HTTP_201_CREATED, content=created_shipment.model_dump()
@@ -69,7 +59,6 @@ def create_shipment(shipment: Shipment):
 
 @shipment_router_v2.put("/{shipment_id}")
 def update_shipment(shipment_id: int, shipment: Shipment):
-
     is_archived = data_provider_v2.fetch_shipment_pool().is_shipment_archived(
         shipment_id
     )
@@ -85,7 +74,6 @@ def update_shipment(shipment_id: int, shipment: Shipment):
 
 @shipment_router_v2.put("/{shipment_id}/orders")
 def update_orders_in_shipment(shipment_id: int, updated_orders: List[Order]):
-
     is_archived = data_provider_v2.fetch_shipment_pool().is_shipment_archived(
         shipment_id
     )
@@ -105,7 +93,6 @@ def update_orders_in_shipment(shipment_id: int, updated_orders: List[Order]):
 
 @shipment_router_v2.put("/{shipment_id}/items")
 def update_items_in_shipment(shipment_id: int, updated_item: List[ItemInObject]):
-
     is_archived = data_provider_v2.fetch_shipment_pool().is_shipment_archived(
         shipment_id
     )
@@ -125,7 +112,6 @@ def update_items_in_shipment(shipment_id: int, updated_item: List[ItemInObject])
 
 @shipment_router_v2.put("/{shipment_id}/commit")
 def commit_shipment(shipment_id: int):
-
     is_archived = data_provider_v2.fetch_shipment_pool().is_shipment_archived(
         shipment_id
     )
@@ -146,7 +132,6 @@ def commit_shipment(shipment_id: int):
 
 @shipment_router_v2.patch("/{shipment_id}")
 def partial_update_shipment(shipment_id: int, shipment: dict):
-
     is_archived = data_provider_v2.fetch_shipment_pool().is_shipment_archived(
         shipment_id
     )
@@ -154,7 +139,6 @@ def partial_update_shipment(shipment_id: int, shipment: dict):
         raise HTTPException(status_code=404, detail="Shipment not found")
     elif is_archived is True:
         raise HTTPException(status_code=400, detail="Shipment is archived")
-
     existing_shipment = data_provider_v2.fetch_shipment_pool().get_shipment(shipment_id)
 
     valid_keys = Shipment.model_fields.keys()
@@ -171,7 +155,6 @@ def partial_update_shipment(shipment_id: int, shipment: dict):
 
 @shipment_router_v2.patch("/{shipment_id}/unarchive")
 def unarchive_shipment(shipment_id: int):
-
     shipment_pool = data_provider_v2.fetch_shipment_pool()
     is_archived = shipment_pool.is_shipment_archived(shipment_id)
     if is_archived is None:
@@ -184,7 +167,6 @@ def unarchive_shipment(shipment_id: int):
 
 @shipment_router_v2.delete("/{shipment_id}")
 def archive_shipment(shipment_id: int):
-
     shipment_pool = data_provider_v2.fetch_shipment_pool()
     is_archived = shipment_pool.is_shipment_archived(shipment_id)
     if is_archived is None:
