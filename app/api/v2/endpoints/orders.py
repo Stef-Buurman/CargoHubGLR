79ff16b733e0a6365fe4cf28d45e79fcfc4b325d
NickWijmans <NickWijmans@gmail.com>
2024-12-19 13:38:18 +0100

    Refactor order endpoints to use request state for pagination and clean up code

--- app/api/v2/endpoints/orders.py
+++ app/api/v2/endpoints/orders.py
@@ -1,17 +1,14 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Request, HTTPException, status
 from fastapi.responses import JSONResponse
-from services.v2.pagination_service import Pagination
 from services.v2 import data_provider_v2
 from models.v2.order import Order
 from models.v2.ItemInObject import ItemInObject
-from utils.globals import pagination_url
 
 order_router_v2 = APIRouter(tags=["v2.Orders"], prefix="/orders")
 
 
 @order_router_v2.get("/{order_id}")
 def read_order(order_id: int):
-
     order = data_provider_v2.fetch_order_pool().get_order(order_id)
     if order is None:
         raise HTTPException(
@@ -21,30 +18,25 @@ def read_order(order_id: int):
 
 
 @order_router_v2.get("/")
-@order_router_v2.get(pagination_url)
-def read_orders(pagination: Pagination = Depends()):
-
+def read_orders(request: Request):
     orders = data_provider_v2.fetch_order_pool().get_orders()
     if orders is None:
         raise HTTPException(status_code=404, detail="Orders not found")
-    return pagination.apply(orders)
+    return request.state.pagination.apply(orders)
 
 
 @order_router_v2.get("/{order_id}/items")
-@order_router_v2.get("/{order_id}/items" + pagination_url)
-def read_order_items(order_id: int, pagination: Pagination = Depends()):
-
+def read_order_items(order_id: int, request: Request):
     items = data_provider_v2.fetch_order_pool().get_items_in_order(order_id)
     if items is None:
         raise HTTPException(
             status_code=404, detail=f"Order with id {order_id} not found"
         )
-    return pagination.apply(items)
+    return request.state.pagination.apply(items)
 
 
 @order_router_v2.post("/")
 def create_order(order: Order):
-
     addedOrder = data_provider_v2.fetch_order_pool().add_order(order)
     if addedOrder is None:
         raise HTTPException(status_code=400, detail="Order has archived entities")
@@ -55,39 +47,33 @@ def create_order(order: Order):
 
 @order_router_v2.put("/{order_id}")
 def update_order(order_id: int, order: Order):
-
     existingOrder = data_provider_v2.fetch_order_pool().is_order_archived(order_id)
     if existingOrder is None:
         raise HTTPException(status_code=404, detail="Order not found")
     elif existingOrder:
         raise HTTPException(status_code=400, detail="Order is archived")
-
     updatedOrder = data_provider_v2.fetch_order_pool().update_order(order_id, order)
     return updatedOrder
 
 
 @order_router_v2.put("/{order_id}/items")
 def add_items_to_order(order_id: int, items: list[ItemInObject]):
-
     existingOrder = data_provider_v2.fetch_order_pool().is_order_archived(order_id)
     if existingOrder is None:
         raise HTTPException(status_code=404, detail="Order not found")
     elif existingOrder:
         raise HTTPException(status_code=400, detail="Order is archived")
-
     data_provider_v2.fetch_order_pool().update_items_in_order(order_id, items)
     return items
 
 
 @order_router_v2.patch("/{order_id}")
 def partial_update_order(order_id: int, order: dict):
-
     existing_order = data_provider_v2.fetch_order_pool().is_order_archived(order_id)
     if existing_order is None:
         raise HTTPException(status_code=404, detail="Order not found")
     elif existing_order:
         raise HTTPException(status_code=400, detail="Order is archived")
-
     existing_order = data_provider_v2.fetch_order_pool().get_order(order_id)
 
     valid_keys = Order.model_fields.keys()
@@ -104,27 +90,21 @@ def partial_update_order(order_id: int, order: dict):
 
 @order_router_v2.delete("/{order_id}")
 def archive_order(order_id: int):
-
     order = data_provider_v2.fetch_order_pool().is_order_archived(order_id)
-
     if order is None:
         raise HTTPException(status_code=404, detail="Order not found")
     elif order:
         raise HTTPException(status_code=400, detail="Order is already archived")
-
     data_provider_v2.fetch_order_pool().archive_order(order_id)
     return {"message": "Order archived successfully"}
 
 
 @order_router_v2.patch("/{order_id}/unarchive")
 def unarchive_order(order_id: int):
-
     order = data_provider_v2.fetch_order_pool().is_order_archived(order_id)
-
     if order is None:
         raise HTTPException(status_code=404, detail="Order not found")
     elif not order:
         raise HTTPException(status_code=400, detail="Order is not archived")
-
     data_provider_v2.fetch_order_pool().unarchive_order(order_id)
     return {"message": "Order unarchived successfully"}
