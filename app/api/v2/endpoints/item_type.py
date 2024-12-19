70116c31c13691cf264370e2f296c9d016931459
NickWijmans <NickWijmans@gmail.com>
2024-12-19 13:54:03 +0100

    Refactor item_type endpoints to use request state for pagination and remove unnecessary blank lines

--- app/api/v2/endpoints/item_type.py
+++ app/api/v2/endpoints/item_type.py
@@ -1,16 +1,14 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Request, HTTPException, status
 from fastapi.responses import JSONResponse
-from services.v2.pagination_service import Pagination
 from services.v2 import data_provider_v2
 from models.v2.item_type import ItemType
-from utils.globals import pagination_url
+
 
 item_type_router_v2 = APIRouter(tags=["v2.Item Types"], prefix="/item_types")
 
 
 @item_type_router_v2.get("/{item_type_id}")
 def read_item_type(item_type_id: int):
-
     item_type = data_provider_v2.fetch_item_type_pool().get_item_type(item_type_id)
     if item_type is None:
         raise HTTPException(
@@ -20,31 +18,26 @@ def read_item_type(item_type_id: int):
 
 
 @item_type_router_v2.get("/")
-@item_type_router_v2.get(pagination_url)
-def read_item_types(pagination: Pagination = Depends()):
-
+def read_item_types(request: Request):
     item_types = data_provider_v2.fetch_item_type_pool().get_item_types()
     if item_types is None:
         raise HTTPException(status_code=404, detail="No item_types found")
-    return pagination.apply(item_types)
+    return request.state.pagination.apply(item_types)
 
 
 @item_type_router_v2.get("/{item_type_id}/items")
-@item_type_router_v2.get("/{item_type_id}/items" + pagination_url)
-def read_items_for_item_type(item_type_id: int, pagination: Pagination = Depends()):
-
+def read_items_for_item_type(item_type_id: int, request: Request):
     item_type = data_provider_v2.fetch_item_type_pool().get_item_type(item_type_id)
     if item_type is None:
         raise HTTPException(
             status_code=404, detail=f"Item_type with id {item_type_id} not found"
         )
     items = data_provider_v2.fetch_item_pool().get_items_for_item_type(item_type_id)
-    return pagination.apply(items)
+    return request.state.pagination.apply(items)
 
 
 @item_type_router_v2.post("/")
 def create_item_type(item_type: ItemType):
-
     added_item_type = data_provider_v2.fetch_item_type_pool().add_item_type(item_type)
     return JSONResponse(
         status_code=status.HTTP_201_CREATED, content=added_item_type.model_dump()
@@ -53,7 +46,6 @@ def create_item_type(item_type: ItemType):
 
 @item_type_router_v2.put("/{item_type_id}")
 def update_item_type(item_type_id: int, item_type: ItemType):
-
     is_archvied = data_provider_v2.fetch_item_type_pool().is_item_type_archived(
         item_type_id
     )
@@ -70,7 +62,6 @@ def update_item_type(item_type_id: int, item_type: ItemType):
 
 @item_type_router_v2.patch("/{item_type_id}")
 def partial_update_item_type(item_type_id: int, item_type: dict):
-
     is_archvied = data_provider_v2.fetch_item_type_pool().is_item_type_archived(
         item_type_id
     )
@@ -99,7 +90,6 @@ def partial_update_item_type(item_type_id: int, item_type: dict):
 
 @item_type_router_v2.patch("/{item_type_id}/unarchive")
 def unarchive_item_type(item_type_id: int):
-
     item_type_pool = data_provider_v2.fetch_item_type_pool()
     is_archived = item_type_pool.is_item_type_archived(item_type_id)
     if is_archived is None:
@@ -113,7 +103,6 @@ def unarchive_item_type(item_type_id: int):
 
 @item_type_router_v2.delete("/{item_type_id}")
 def archive_item_type(item_type_id: int):
-
     item_type_pool = data_provider_v2.fetch_item_type_pool()
     is_archived = item_type_pool.is_item_type_archived(item_type_id)
     if is_archived is None:
