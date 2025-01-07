7f938a9f5ff844b13f51afbf026b28d5990bd0d6
NickWijmans <nickwijmans@gmail.com>
2025-01-07 16:46:07 +0100

    Started implementing logging middleware for orders/shipments/users

--- app/api/v2/logging_middleware.py
+++ app/api/v2/logging_middleware.py
@@ -3,8 +3,6 @@ import json
 import os
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.requests import Request
-
-# from services.v1 import data_provider
 from services.v2 import data_provider_v2
 
 PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.."))
@@ -17,7 +15,6 @@ info_logger = logging.getLogger("infoLogger")
 info_logger.setLevel(logging.INFO)
 info_logger.propagate = False
 
-
 info_file_handler = logging.FileHandler(PROJECT_PATH + "/logs/application.log")
 info_file_handler.setLevel(logging.INFO)
 info_file_handler.setFormatter(
@@ -25,19 +22,9 @@ info_file_handler.setFormatter(
 )
 info_logger.addHandler(info_file_handler)
 
-
-info_stream_handler = logging.StreamHandler()
-info_stream_handler.setLevel(logging.INFO)
-info_stream_handler.setFormatter(
-    logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-)
-info_logger.addHandler(info_stream_handler)
-
-
 error_logger = logging.getLogger("errorLogger")
 error_logger.setLevel(logging.ERROR)
 error_logger.propagate = False
-
 
 error_file_handler = logging.FileHandler(PROJECT_PATH + "/logs/error.log")
 error_file_handler.setLevel(logging.ERROR)
@@ -47,19 +34,10 @@ error_file_handler.setFormatter(
 error_logger.addHandler(error_file_handler)
 
 
-error_stream_handler = logging.StreamHandler()
-error_stream_handler.setLevel(logging.ERROR)
-error_stream_handler.setFormatter(
-    logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-)
-error_logger.addHandler(error_stream_handler)
-
-
 async def get_previous_data(request: Request):
     path_parts = request.url.path.split("/")
 
     if len(path_parts) > 2:
-        # version = path_parts[2] if len(path_parts) > 2 else None
         resource_type = path_parts[3] if len(path_parts) > 3 else None
         id = path_parts[4] if len(path_parts) > 4 else None
 
@@ -91,162 +69,85 @@ async def get_previous_data(request: Request):
 
 class LoggingProviderMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
+        if request.method in ["POST", "PUT", "PATCH", "DELETE"] and any(
+            resource in request.url.path
+            for resource in ["users", "orders", "shipments"]
+        ):
 
-        info_logger.info(f"Request: {request.method} {request.url.path}")
+            info_logger.info(f"Request: {request.method} {request.url.path}")
 
-        try:
+            try:
+                request_body = await request.body()
+                if request_body:
+                    body = json.loads(request_body)
+                    if isinstance(body, dict):
+                        info_logger.info(f"Source_id: {body.get('source_id')}")
+                    else:
+                        info_logger.warning("Request body is not a dictionary")
+            except json.JSONDecodeError:
+                info_logger.warning("Failed to parse request body as JSON")
 
-            request_body = await request.body()
-            if request_body:
-                body = json.loads(request_body)
-                if isinstance(body, dict):
-                    info_logger.info(f"Source_id: {body.get('source_id')}")
-                else:
-                    info_logger.warning("Request body is not a dictionary")
-        except json.JSONDecodeError:
-            info_logger.warning("Failed to parse request body as JSON")
+            try:
+                previous_data = await get_previous_data(request)
+                response = await call_next(request)
+                response_body = b"".join(
+                    [section async for section in response.body_iterator]
+                )
+                path_parts = request.url.path.split("/")
 
-        try:
-            if request.method == "POST":
-                info_logger.info("Previous Data: None")
-            elif request.method in ["PUT"]:
-                try:
-                    previous_data = await get_previous_data(request)
+                async def new_body_iterator():
+                    yield response_body
+
+                response.body_iterator = new_body_iterator()
+
+                if request.method == "POST":
+                    info_logger.info("Previous Data: None")
+                    info_logger.info(f"New Data: {response_body.decode('utf-8')}")
+                elif request.method in ["PUT", "PATCH"] and len(path_parts) <= 5:
                     request_body = await request.json()
+                    updated_fields = json.loads(response_body.decode("utf-8"))
                     if isinstance(request_body, dict):
                         filtered_previous_data = {
                             k: getattr(previous_data, k, None)
                             for k in request_body.keys()
-                            if k != "source_id"
-                            and k != "id"
+                            if k not in ["source_id", "id"]
                             and getattr(previous_data, k, None) != request_body[k]
+                        }
+                        filtered_updated_fields = {
+                            k: updated_fields.get(k)
+                            for k in request_body.keys()
+                            if k not in ["source_id", "id"]
+                            and getattr(previous_data, k, None) != updated_fields.get(k)
                         }
                     else:
                         filtered_previous_data = {}
-                    info_logger.info(
-                        f"Previous Data: {json.dumps(filtered_previous_data, default=lambda o: o.__dict__)}"
-                    )
-                except Exception as e:
-                    error_logger.error(
-                        f"Error occurred while fetching previous data: {e}",
-                        exc_info=True,
-                    )
-            elif request.method == "PATCH":
-                try:
-                    path_parts = request.url.path.split("/")
-                    request_body = await request.json()
-                    previous_data = await get_previous_data(request)
+                        filtered_updated_fields = {}
 
-                    if len(path_parts) > 5 and path_parts[5] == "unarchive":
-                        filtered_previous_data = {
-                            "is_archived": getattr(previous_data, "is_archived", None)
-                        }
-                    else:
-                        filtered_previous_data = {
-                            k: getattr(previous_data, k, None)
-                            for k in request_body.keys()
-                            if k != "source_id"
-                        }
                     info_logger.info(
                         f"Previous Data: {json.dumps(filtered_previous_data, default=lambda o: o.__dict__)}"
                     )
-                except Exception as e:
-                    error_logger.error(
-                        f"Error occurred while fetching previous data: {e}",
-                        exc_info=True,
-                    )
-            elif request.method == "DELETE":
-                try:
-                    previous_data = await get_previous_data(request)
+                    info_logger.info(f"New Data: {json.dumps(filtered_updated_fields)}")
+                elif request.method == "DELETE" or (
+                    request.method == "PATCH"
+                    and len(path_parts) >= 6
+                    and path_parts[5] == "unarchive"
+                ):
+                    deleted_data = json.loads(response_body.decode("utf-8"))
                     is_archived_value = {
-                        "is_archived": getattr(previous_data, "is_archived", None)
+                        "is_archived": deleted_data.get("is_archived", None)
                     }
-                    info_logger.info(f"Previous Data: {json.dumps(is_archived_value)}")
-                except Exception as e:
-                    error_logger.error(
-                        f"Error occurred while fetching previous data: {e}",
-                        exc_info=True,
+                    info_logger.info(
+                        f"Previous Data: {json.dumps({'is_archived': getattr(previous_data, 'is_archived', None)})}"
                     )
-            previous_data = await get_previous_data(request)
+                    info_logger.info(f"New Data: {json.dumps(is_archived_value)}")
+
+                info_logger.info(f"Response: {response.status_code}")
+                return response
+            except Exception as e:
+                error_logger.error(
+                    f"Error occurred while processing the request: {e}", exc_info=True
+                )
+                raise e
+        else:
             response = await call_next(request)
-
-            if request.method in ["POST"]:
-                response_body = b"".join(
-                    [section async for section in response.body_iterator]
-                )
-
-                async def new_body_iterator():
-                    yield response_body
-
-                response.body_iterator = new_body_iterator()
-                info_logger.info(f"New Data: {response_body.decode('utf-8')}")
-                info_logger.info(f"Response: {response.status_code}")
-            elif request.method == "PUT":
-                response_body = b"".join(
-                    [section async for section in response.body_iterator]
-                )
-
-                async def new_body_iterator():
-                    yield response_body
-
-                response.body_iterator = new_body_iterator()
-                updated_fields = json.loads(response_body.decode("utf-8"))
-                if isinstance(request_body, dict):
-                    filtered_updated_fields = {
-                        k: updated_fields.get(k)
-                        for k in request_body.keys()
-                        if k != "source_id"
-                        and k != "id"
-                        and getattr(previous_data, k, None) != updated_fields.get(k)
-                    }
-                else:
-                    filtered_updated_fields = {}
-                info_logger.info(f"New Data: {json.dumps(filtered_updated_fields)}")
-                info_logger.info(f"Response: {response.status_code}")
-            elif request.method == "PATCH":
-                response_body = b"".join(
-                    [section async for section in response.body_iterator]
-                )
-
-                async def new_body_iterator():
-                    yield response_body
-
-                response.body_iterator = new_body_iterator()
-                updated_fields = json.loads(response_body.decode("utf-8"))
-                request_body = await request.json()
-                path_parts = request.url.path.split("/")
-
-                if len(path_parts) > 5 and path_parts[5] == "unarchive":
-                    filtered_updated_fields = {
-                        "is_archived": updated_fields.get("is_archived", None)
-                    }
-                else:
-                    filtered_updated_fields = {
-                        k: updated_fields.get(k)
-                        for k in request_body.keys()
-                        if k != "source_id"
-                    }
-                info_logger.info(f"New Data: {json.dumps(filtered_updated_fields)}")
-                info_logger.info(f"Response: {response.status_code}")
-            elif request.method == "DELETE":
-                response_body = b"".join(
-                    [section async for section in response.body_iterator]
-                )
-
-                async def new_body_iterator():
-                    yield response_body
-
-                response.body_iterator = new_body_iterator()
-                deleted_data = json.loads(response_body.decode("utf-8"))
-                is_archived_value = {
-                    "is_archived": deleted_data.get("is_archived", None)
-                }
-                info_logger.info(f"New Data: {json.dumps(is_archived_value)}")
-                info_logger.info(f"Response: {response.status_code}")
             return response
-        except Exception as e:
-
-            error_logger.error(
-                f"Error occurred while processing the request: {e}", exc_info=True
-            )
-            raise e
