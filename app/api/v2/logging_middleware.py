dc2df0d4e8f41530f9646bf8aeeda616d06591e8
NickWijmans <nickwijmans@gmail.com>
2025-01-07 16:46:07 +0100

    Started implementing logging middleware for orders/shipments/users

---
+++ app/api/v2/logging_middleware.py
@@ -0,0 +1,82 @@
+import logging
+import json
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.requests import Request
+
+
+info_logger = logging.getLogger("infoLogger")
+info_logger.setLevel(logging.INFO)
+info_logger.propagate = False
+
+
+info_file_handler = logging.FileHandler("application.log")
+info_file_handler.setLevel(logging.INFO)
+info_file_handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+)
+info_logger.addHandler(info_file_handler)
+
+
+info_stream_handler = logging.StreamHandler()
+info_stream_handler.setLevel(logging.INFO)
+info_stream_handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+)
+info_logger.addHandler(info_stream_handler)
+
+
+error_logger = logging.getLogger("errorLogger")
+error_logger.setLevel(logging.ERROR)
+error_logger.propagate = False
+
+
+error_file_handler = logging.FileHandler("error.log")
+error_file_handler.setLevel(logging.ERROR)
+error_file_handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+)
+error_logger.addHandler(error_file_handler)
+
+
+error_stream_handler = logging.StreamHandler()
+error_stream_handler.setLevel(logging.ERROR)
+error_stream_handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+)
+error_logger.addHandler(error_stream_handler)
+
+
+class LoggingProviderMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+
+        if (
+            any(
+                keyword in request.url.path for keyword in ["order", "shipment", "user"]
+            )
+            and request.method != "GET"
+        ):
+            info_logger.info(f"Request: {request.method} {request.url}")
+
+            try:
+
+                request_body = await request.body()
+                if request_body:
+                    body = json.loads(request_body)
+                    info_logger.info(f"SourceId: {body.get('sourceId')}")
+            except json.JSONDecodeError:
+                info_logger.warning("Failed to parse request body as JSON")
+
+            try:
+
+                response = await call_next(request)
+                info_logger.info(f"Response: {response.status_code}")
+                return response
+            except Exception as e:
+
+                error_logger.error(
+                    f"Error occurred while processing the request: {e}", exc_info=True
+                )
+                raise e
+        else:
+
+            return await call_next(request)
