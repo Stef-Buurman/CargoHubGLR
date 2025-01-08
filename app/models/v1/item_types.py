5fe351e4e737868a3168e8917a81dbea39b1591a
Stef Buurman <St3fbuurman@gmail.com>
2025-01-08 13:22:42 +0100

    Refactor add_item_type and update_item_type methods to utilize data_provider_v2 and return created item type

--- app/models/v1/item_types.py
+++ app/models/v1/item_types.py
@@ -1,6 +1,8 @@
 import json
+from models.v2.item_type import ItemType
 
 from .base import Base
+from services.v2 import data_provider_v2
 
 ITEM_TYPES = []
 
@@ -20,21 +22,24 @@ class ItemTypes(Base):
         return None
 
     def add_item_type(self, item_type):
-        item_type["created_at"] = self.get_timestamp()
-        item_type["updated_at"] = self.get_timestamp()
-        self.data.append(item_type)
+        created_item_type = data_provider_v2.fetch_item_type_pool().add_item_type(ItemType(**item_type))
+        return created_item_type.model_dump()
 
     def update_item_type(self, item_type_id, item_type):
         item_type["updated_at"] = self.get_timestamp()
         for i in range(len(self.data)):
             if self.data[i]["id"] == item_type_id:
-                self.data[i] = item_type
+                item_type["id"] = item_type_id
+                item_type["created_at"] = self.data[i]["created_at"]
+                # self.data[i] = item_type
+                data_provider_v2.fetch_item_type_pool().update_item_type(item_type_id, ItemType(**item_type))
                 break
 
     def remove_item_type(self, item_type_id):
         for x in self.data:
             if x["id"] == item_type_id:
                 self.data.remove(x)
+                data_provider_v2.fetch_item_type_pool().delete_item_type(item_type_id)
 
     def load(self, is_debug):
         if is_debug:
@@ -44,7 +49,9 @@ class ItemTypes(Base):
             self.data = json.load(f)
             f.close()
 
-    def save(self):  # pragma: no cover
+    def save(self, data = None):  # pragma: no cover
+        if data:
+            self.data = data
         f = open(self.data_path, "w")
         json.dump(self.data, f)
         f.close()
