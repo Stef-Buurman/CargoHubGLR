309734986c1e7c0313959f7fb51f49207740a17b
Stef Buurman <St3fbuurman@gmail.com>
2025-01-08 19:00:25 +0100

    Remove unnecessary newline in item_types.py

--- app/models/v1/item_types.py
+++ app/models/v1/item_types.py
@@ -53,6 +53,7 @@ class ItemTypes(Base):
                     )
                     return updated_item_type.model_dump()
 
+
     def remove_item_type(self, item_type_id):
         for x in self.data:
             if x["id"] == item_type_id:
