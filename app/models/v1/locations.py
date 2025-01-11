d834dd59f610d3e769e08f37ab979c7aaa96e440
NickWijmans <NickWijmans@gmail.com>
2025-01-11 15:09:22 +0100

    Refactor location management to support v2 data provider and improve response handling

--- app/models/v1/locations.py
+++ app/models/v1/locations.py
@@ -1,12 +1,15 @@
 import json
+from models.v2.location import Location
 
 from .base import Base
+from services.v2 import data_provider_v2
 
 LOCATIONS = []
 
 
 class Locations(Base):
     def __init__(self, root_path, is_debug=False):
+        self.is_debug = is_debug
         self.data_path = root_path + "locations.json"
         self.load(is_debug)
 
@@ -27,21 +30,41 @@ class Locations(Base):
         return result
 
     def add_location(self, location):
-        location["created_at"] = self.get_timestamp()
-        location["updated_at"] = self.get_timestamp()
-        self.data.append(location)
+        if self.is_debug:
+            location["created_at"] = self.get_timestamp()
+            location["updated_at"] = self.get_timestamp()
+            self.data.append(location)
+            return location
+        else:
+            created_location = data_provider_v2.fetch_location_pool().add_location(
+                **location
+            )
+            return created_location.model_dump()
 
     def update_location(self, location_id, location):
         location["updated_at"] = self.get_timestamp()
         for i in range(len(self.data)):
             if self.data[i]["id"] == location_id:
-                self.data[i] = location
-                break
+                location["id"] = location_id
+                if location.get("created_at") is None:
+                    location["created_at"] = self.data[i]["created_at"]
+                if self.is_debug:
+                    self.data[i] = location
+                    return location
+                else:
+                    updated_location = (
+                        data_provider_v2.fetch_location_pool().update_location(
+                            location_id, **location
+                        )
+                    )
+                    return updated_location.model_dump()
 
     def remove_location(self, location_id):
         for x in self.data:
             if x["id"] == location_id:
                 self.data.remove(x)
+                if not self.is_debug:
+                    data_provider_v2.fetch_location_pool().archive_location(location_id)
 
     def load(self, is_debug):
         if is_debug:
@@ -51,7 +74,9 @@ class Locations(Base):
             self.data = json.load(f)
             f.close()
 
-    def save(self):  # pragma: no cover
+    def save(self, data=None):  # pragma: no cover
+        if data:
+            self.data = data
         f = open(self.data_path, "w")
         json.dump(self.data, f)
         f.close()
