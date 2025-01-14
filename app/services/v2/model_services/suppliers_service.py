3a691aaf5059fd8d4e3861327a3e854ac9ac1020
Stef Buurman <St3fbuurman@gmail.com>
2025-01-14 11:31:53 +0100

    Integrate background task fetching into supplier service save method

--- app/services/v2/model_services/suppliers_service.py
+++ app/services/v2/model_services/suppliers_service.py
@@ -1,3 +1,4 @@
+from services.v2 import data_provider_v2
 from models.v2.supplier import Supplier
 from typing import List, Type
 from services.v2.base_service import Base
@@ -76,9 +77,9 @@ class SupplierService(Base):
 
     def save(self):
         if not self.is_debug:
-            data_provider.fetch_supplier_pool().save(
+            data_provider_v2.fetch_background_tasks(data_provider.fetch_supplier_pool().save(
                 [supplier.model_dump() for supplier in self.data]
-            )
+            ))
 
     def load(self):
         self.data = self.get_all_suppliers()
