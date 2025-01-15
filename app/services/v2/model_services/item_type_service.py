d3bf174fa114d66dea6d54d57fcce61b5d7979d5
Stef Buurman <St3fbuurman@gmail.com>
2025-01-15 10:46:32 +0100

    Refactor save method in ItemTypeService to use a named function for background task execution

--- app/services/v2/model_services/item_type_service.py
+++ app/services/v2/model_services/item_type_service.py
@@ -81,11 +81,11 @@ class ItemTypeService(Base):
 
     def save(self):
         if not self.is_debug:
-            data_provider_v2.fetch_background_tasks().add_task(
+            def call_v1_save_method():
                 data_provider.fetch_item_type_pool().save(
                     [item.model_dump() for item in self.data]
                 )
-            )
+            data_provider_v2.fetch_background_tasks().add_task(call_v1_save_method)
 
     def load(self):
         self.data = self.get_all_item_types()
